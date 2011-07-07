8432028886c830ecf1009e65ed72ac012e86e583
Olivier Dolbeau <olivier.dolbeau@gmail.com>
2011-07-08 00:03:59 +0200

    Started XML Indentation working

--- src/Olitaz/ToolboxBundle/Entity/Xml.php
+++ src/Olitaz/ToolboxBundle/Entity/Xml.php
@@ -7,6 +7,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Xml
 {
 
+    const INDENTATION = "X";
+
     /**
      * @Assert\NotBlank()
      */
@@ -21,22 +23,47 @@ class Xml
     {
         $this->content = $content;
     }
- 
+
     public function process() {
         try {
             $xml = new \XMLReader();
             $xml->xml($this->content);
-            $xml->setParserProperty(\XMLReader::VALIDATE, true);
-            if($xml->isValid()) {
-                while($xml->read()) {
-
-                }
-            } else {
-                return false;
-            }
+            return $this->formatXml($xml, 0);
         } catch(\Exception $e) {
             return $e->getMessage();
         }
+    }
+
+    protected function formatXml(\XMLReader $xml, $level= 0) {
+        echo $level.'<br />';
+        $indentedXml = '';
+        while($xml->read()) {
+            switch ($xml->nodeType) {
+                case \XMLReader::ELEMENT:
+                    $nodeName = $xml->localName;
+                    $indentedXml .= $this->getIndentation($level) . '<' . $nodeName . ' ';
+                    if($xml->hasAttributes()) {
+                        while($xml->moveToNextAttribute()) {
+                            
+                        }
+                    }
+                    $indentedXml .= $this->formatXml($xml, $level+1);
+                    $indentedXml .= $this->getIndentation($level) . '</' . $nodeName . '><br />';
+                    break;
+                case \XMLReader::END_ELEMENT:
+                    return $indentedXml;
+                    break;
+            }
+        }
+        return $indentedXml;
+    }
+
+    protected function getIndentation($level = 0) {
+        $indentation = '';
+        for($i=0; $i<$level; $i++) {
+            $indentation .= self::INDENTATION;
+        }
+        return $indentation;
     }
 
 }
